9083dc14804da37e9689021d321d108bb952282f
Christopher Kaster <ikasoki@gmail.com>
2015-12-30 16:32:17 +0100

    add ability to reset and some slight improvements on visuals

--- js/main.js
+++ js/main.js
@@ -1,28 +1,67 @@
-var data = {
-    player1: 20,
-    player2: 20
-};
+function init() {
+    if(!exists("player-one") && !exists("player-two")) {
+        reset();
+    }
+
+    set("player-one", get("player-one"));
+    set("player-two", get("player-two"));
+}
+
+function reset() {
+    set("player-one", "20");
+    set("player-two", "20");
+}
+
+function exists(name) {
+    return Boolean(localStorage.getItem(name));
+}
+
+function set(name, value) {
+    localStorage.setItem(name, "" + value);
+    $("." + name + " .odometer").html(value);
+}
+
+function get(name) {
+    return Number(localStorage.getItem(name));
+}
+
+function add(name) {
+    var val = get(name);
+    set(name, ++val);
+}
+
+function sub(name) {
+    var val = get(name);
+    set(name, --val);
+}
 
 $(document).ready(function() {
-    $(".menu-button a").bigSlide();
+    var menuButton = $(".menu-button a").bigSlide({
+        easyClose: true
+    });
 
+    init();
+
+    // set up +/- buttons
     $(".player-one-plus").click(function() {
-        data.player1++;
-        $(".player-one .odometer").html(data.player1);
+        add("player-one");
     });
 
     $(".player-one-minus").click(function() {
-        data.player1--;
-        $(".player-one .odometer").html(data.player1);
+        sub("player-one");
     });
 
     $(".player-two-plus").click(function() {
-        data.player2++;
-        $(".player-two .odometer").html(data.player2);
+        add("player-two");
     });
 
     $(".player-two-minus").click(function() {
-        data.player2--;
-        $(".player-two .odometer").html(data.player2);
+        sub("player-two");
     });
+
+    // setup menu buttons
+    $("#reset-button").click(function() {
+        reset();
+        $(menuButton).click();
+    })
 });
